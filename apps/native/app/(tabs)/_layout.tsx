15340f4af0fb1f26a3451f440a5aeced6da2e1fe
mrevanzak <mrevanzak@gmail.com>
2025-12-24 21:19:10 +0700

    feat: initial commit

---
+++ apps/native/app/(tabs)/_layout.tsx
@@ -0,0 +1,40 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { HapticTab } from "@/relune/apps/native/components/haptic-tab";
+import { IconSymbol } from "@/relune/apps/native/components/ui/icon-symbol";
+import { Colors } from "@/relune/apps/native/constants/theme";
+import { useColorScheme } from "@/relune/apps/native/hooks/use-color-scheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
